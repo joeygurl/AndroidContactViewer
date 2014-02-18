a7f6bf84e413eea4ca5d1f1e38c6fbd217e705fd
Diorelle Ramos <dramos30@gmail.com>
2014-02-18 14:07:42 -0600

    test commit

--- ContactViewer/src/main/java/com/umnappclass/contactviewer/TestActivity.java
+++ ContactViewer/src/main/java/com/umnappclass/contactviewer/TestActivity.java
@@ -4,4 +4,10 @@ package com.umnappclass.contactviewer;
  * Created by dramos on 2/18/14.
  */
 public class TestActivity {
+    String testName;
+
+    public TestActivity (String t)
+    {
+        testName = t;
+    }
 }
